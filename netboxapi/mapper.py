a7c12762ef87d308aed07e103d6a944e1d62e396
Anthony Ruhier <anthony.ruhier@trinaps.com>
2017-03-27 15:02:00 +0200

    Add documentation for mapper

--- netboxapi/mapper.py
+++ netboxapi/mapper.py
@@ -13,6 +13,20 @@ class NetboxMapper():
         )
 
     def get(self, *args, **kwargs):
+        """
+        Get netbox objects
+
+        Use args and kwargs as filters: all *args items are joined with "/",
+        and kwargs are use as data parameter. It is then used to build the
+        request uri
+
+        Examples:
+            >>> netbox_mapper.app_name = "dcim"
+            >>> netbox_mapper.model = "sites"
+            >>> netbox_mapper.get("1", "racks", q="name_to_filter")
+
+            Will do a request to "/api/dcim/sites/1/racks/?q=name_to_filter"
+        """
         url = self._url + "/".join(str(a) for a in args)
 
         new_mappers_dict = self.netbox_api.get(url, data=kwargs)
